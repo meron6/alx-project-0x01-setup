7b2431762508becba7b97f25ce89cd005a814e5e
meron6 <khozameron5@gmail.com>
2025-10-01 12:54:35 +0000

    update

--- alx-project-0x01/pages/index.tsx
+++ alx-project-0x01/pages/index.tsx
@@ -1,1 +1,16 @@
-pages/index.tsx doesn't exist
+import Header from "../components/layout/Header";
+import Footer from "../components/layout/Footer";
+
+const Home: React.FC = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex flex-1 justify-center items-center">
+        <h1 className="text-7xl font-thin">Welcome to our Application!</h1>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
